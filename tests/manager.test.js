7e089007a9932565c6bc8e405c4755e55f090d83
ima2132 <ima2132@columbia.edu>
2023-05-23 16:34:17 -0400

    added to manager.test.js; now onto finish the rest of tests files

---
+++ tests/manager.test.js
@@ -0,0 +1,24 @@
+const Manager = require('../lib/Manager');
+
+describe('Manager', () => {
+    it('should create a Manager object with name, id, email, and office number', () => {
+        const manager = new Manager('Aang', 1, 'aang@avatar.com', 'A1');
+
+        expect(manager.name).toEqual('Aang');
+        expect(manager.id).toEqual(1);
+        expect(manager.email).toEqual('aang@avatar.com');
+        expect(manager.officeNumber).toEqual('A1');
+    });
+
+    it('getRole() should return "Manager"', () => {
+        const manager = new Manager('Aang', 1, 'aang@avatar.com', 'A1');
+
+        expect(manager.getRole()).toEqual('Manager');
+    });
+
+    it('getOfficeNumber() should return the office number of the Manager', () => {
+        const manager = new Manager('Aang', 1, 'aang@avatar.com', 'A1');
+
+        expect(manager.getOfficeNumber()).toEqual('A1');
+    });
+});
